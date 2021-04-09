d93b6e949332443e35c78120c3fbb579d510a9e1
Arjun C  Menon <55993011+justArno@users.noreply.github.com>
2021-04-09 19:44:33 +0530

    Split functions

---
+++ Resume Parser/name.py
@@ -0,0 +1,24 @@
+
+import nltk
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+
+
+
+def extract_names(txt):
+    person_names = []
+
+    for sent in nltk.sent_tokenize(txt):
+        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+            if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
+                person_names.append(
+                    ' '.join(chunk_leave[0] for chunk_leave in chunk.leaves())
+                )
+
+    return person_names
+
+
